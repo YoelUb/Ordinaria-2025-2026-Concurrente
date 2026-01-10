bec0a3140e02c3e076bca144762f094a4dcc3bce
YoelUb <yurqubar@myuax.com>
2026-01-10 19:05:45 +0100

    mejorar el front

---
+++ src/schemas/user_schema.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+from datetime import datetime
+
+
+# Propiedades compartidas
+class UserBase(BaseModel):
+    email: EmailStr
+    full_name: Optional[str] = None
+    apartment: Optional[str] = None
+    phone: Optional[str] = None
+    is_active: bool = True
+
+
+# Propiedades para recibir vía API al crear con password
+class UserCreate(UserBase):
+    password: str
+
+
+# Propiedades para devolver vía API sin password
+class UserResponse(UserBase):
+    id: int
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
